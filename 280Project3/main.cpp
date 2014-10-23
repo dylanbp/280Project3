00432fd1c33bad44c49204d517dd24ccc0dc6b76
dylanbp <dylanphillips@Macintosh.local>
2014-10-22 21:59:39 -0400

    Initial Commit

---
+++ 280Project3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  280Project3
+//
+//  Created by dylanbp on 10/22/14.
+//  Copyright (c) 2014 University of Michigan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
